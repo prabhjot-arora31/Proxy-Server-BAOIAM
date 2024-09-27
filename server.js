32b2d8e39b4b2e66b6dda15c7b749c844b7afaee
prabhjot-arora31 <prabhjotarora99@gmail.com>
2024-09-27 22:54:46 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+const port = 3000;
+
+// Enable CORS
+app.use(cors());
+
+// JSON parser
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Working....");
+});
+// Proxy route to handle Google Apps Script request
+app.post("/submit-form", async (req, res) => {
+  const data = req.body;
+
+  try {
+    console.log("data is:", data);
+    const response = await axios.post(
+      "https://script.google.com/macros/s/AKfycbx4s1dMZkppjnTgQ1Y7ml9ZI28ySXraFsjJeVThhOKhxeLRiHd908s3rsaoNOZnO1E3/exec",
+      data
+    );
+    console.log("get here");
+    res.json(response.data); // Forward the response to the client
+  } catch (error) {
+    console.error("Error forwarding the request", error);
+    res.status(500).send("Error submitting form");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Proxy server running on http://localhost:${port}`);
+});
+module.exports = app;
