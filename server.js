3e4ea4a18dfbcec7fff4ae1e4ddb35327b201bcc
prabhjot-arora31 <prabhjotarora99@gmail.com>
2024-09-28 20:54:57 +0530

    modifying contact-form

--- server.js
+++ server.js
@@ -20,10 +20,11 @@ app.post("/submit-form", async (req, res) => {
   try {
     console.log("data is:", data);
     const response = await axios.post(
-      "https://script.google.com/macros/s/AKfycbwLzR4IZceFm2yPH9HeHFfTW28P53ljrqQQJk6LGTDi-eDNXYEwcADfW9q9ElFXeBZx/exec",
+      "https://script.google.com/macros/s/AKfycby9tY8O7FwZfAzzpArMtBuLWR2uW9AtnloQKNZz5rn7jbC2255IDo_aZbq8vpbq-dc/exec",
       data
     );
     console.log("get here");
+    console.log("in submit-form:", response.data);
     res.status(200).json(response.data); // Forward the response to the client
   } catch (error) {
     console.error("Error forwarding the request", error);
@@ -36,7 +37,7 @@ app.post("/contact-form", async (req, res) => {
   try {
     console.log("data is:", data);
     const response = await axios.post(
-      "https://script.google.com/macros/s/AKfycbweV43z0PGUYDSNBVJEfyut-BPPbbKFdQaRNbUevICiRCKPlRVWAVH-Cde_l2GcwPhl/exec",
+      "https://script.google.com/macros/s/AKfycby0IO6oS_XoQEV_sl6tMsj7-nr_r2A7gc1RA7kOJFwXNoGRwQHyrCQt2GBpYAPKu2YwrA/exec",
       data
     );
     console.log("get here (updated)");
