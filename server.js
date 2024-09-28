83f0e7c0c5173364def931b9a5de4025b5089460
prabhjot-arora31 <prabhjotarora99@gmail.com>
2024-09-28 12:12:02 +0530

    adding logic for contact form

--- server.js
+++ server.js
@@ -24,13 +24,29 @@ app.post("/submit-form", async (req, res) => {
       data
     );
     console.log("get here");
-    res.json(response.data); // Forward the response to the client
+    res.status(200).json(response.data); // Forward the response to the client
   } catch (error) {
     console.error("Error forwarding the request", error);
     res.status(500).send("Error submitting form");
   }
 });
+app.post("/contact-form", async (req, res) => {
+  const data = req.body;
 
+  try {
+    console.log("data is:", data);
+    const response = await axios.post(
+      "https://script.google.com/macros/s/AKfycbweV43z0PGUYDSNBVJEfyut-BPPbbKFdQaRNbUevICiRCKPlRVWAVH-Cde_l2GcwPhl/exec",
+      data
+    );
+    console.log("get here (updated)");
+    console.log("res is:", response.data);
+    res.status(200).json(response.data); // Forward the response to the client
+  } catch (error) {
+    console.error("Error forwarding the request", error);
+    res.status(500).send("Error submitting form");
+  }
+});
 app.listen(port, () => {
   console.log(`Proxy server running on http://localhost:${port}`);
 });
